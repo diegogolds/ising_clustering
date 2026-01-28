44893b5b46c8fd05ebd506e53ceb8cdc2dc14877
diegogolds <83447505+diegogolds@users.noreply.github.com>
2026-01-28 14:03:29 -0300

    Delete Parameter selection/Kostia visit directory

--- Parameter selection/Kostia visit/alpha_bounds.m
+++
@@ -1,7 +0,0 @@
-v1 = 1;
-v2 = 0.75;
-a = 7;
-b = 1;
-
-alpha_min = (b * v1 - a * v2) / (v1 - v2)
-alpha_max = (a * v1 - b * v2) / (v1 - v2)
--- Parameter selection/Kostia visit/energy_plot.m
+++
@@ -1,3 +0,0 @@
-[z1, z2] = meshgrid(-1 : 0.01 : 1, -1 : 0.01 : 1);
-h = hamiltonian(z1, z2);
-surf(z1, z2, h);
